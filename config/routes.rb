cf615fea7bce21ec2b5b771bf01b3c7506f3e2f3
Marco Crepaldi <marco.crepaldi@gmail.com>
2018-04-11 10:25:38 +0200

    enable graphiql to admins for production

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
 end
 
 Binda::Engine.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 end
